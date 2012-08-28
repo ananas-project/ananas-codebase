0e436593582ee4361a43d023731a6d4b96362c3c
xukun0217 <xukun0217@gmail.com>
2012-08-28 09:39:45 +0800

    ...

--- lib/blueprint/test/test/blueprint/MyResourceIdGen.java
+++ lib/blueprint/test/test/blueprint/MyResourceIdGen.java
@@ -4,6 +4,12 @@ import ananas.lib.blueprint.tools.ResourceIdGen;
 
 public class MyResourceIdGen {
 
+	/**
+	 * @param arg
+	 *            in command-line call this with the parameter of
+	 *            "-base-dir ${project_loc} -res-dir res -gen-dir gen -R-class test.blueprint.R -accept-file .xml+.png"
+	 * */
+
 	public static void main(String[] arg) {
 		ResourceIdGen.main(arg);
 	}
