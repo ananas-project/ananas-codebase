b702cc7858b46f27883fe0939cf2e539db0b4de6
ak17 <xukun0217@gmail.com>
2012-08-29 22:36:43 +0800

    add "gen" directory

--- lib/blueprint/test/test/blueprint/MyResourceIdGen.java
+++ lib/blueprint/test/test/blueprint/MyResourceIdGen.java
@@ -7,7 +7,7 @@ public class MyResourceIdGen {
 	/**
 	 * @param arg
 	 *            in command-line call this with the parameter of
-	 *            "-base-dir ${project_loc} -res-dir res -gen-dir gen -R-class test.blueprint.R -accept-file .xml+.png"
+	 *            "-base-dir ${project_loc} -res-dir test -gen-dir gen -R-class test.blueprint.R -accept-file .xml+.png -accept-attr id"
 	 * */
 
 	public static void main(String[] arg) {
