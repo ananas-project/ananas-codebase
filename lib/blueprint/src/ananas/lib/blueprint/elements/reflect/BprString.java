6533eb61abbe61da4357d7186675066b28f3aab7
xukun <xukun@xukun-asus>
2012-09-23 22:11:37 +0800

    modify:BprString parse text data

--- lib/blueprint/src/ananas/lib/blueprint/elements/reflect/BprString.java
+++ lib/blueprint/src/ananas/lib/blueprint/elements/reflect/BprString.java
@@ -2,14 +2,28 @@ package ananas.lib.blueprint.elements.reflect;
 
 public class BprString extends BprBaseType {
 
+	private StringBuilder mStrBldr = new StringBuilder();
 	private String mValue;
 
 	@Override
 	protected void parseString(String s) {
-		this.mValue = s;
+		// this.mValue = s;
 	}
 
 	public String stringValue() {
+		if (this.mValue == null) {
+			this.mValue = this.mStrBldr.toString().trim();
+		}
 		return this.mValue;
 	}
+
+	public boolean bind(Object child) {
+		if (child instanceof String) {
+			this.mStrBldr.append(child.toString());
+			return true;
+		} else {
+			return super.bind(child);
+		}
+	}
+
 }
